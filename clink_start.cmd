0853bfaf264d391385fcfcfe33aca3765089d2aa
Stachowiak, Bartosz <bartosz.stachowiak@aptiv.com>
2024-12-31 14:07:31 +0100

    add initial clink configuration

---
+++ clink_start.cmd
@@ -0,0 +1,19 @@
+:: Get current directory
+@echo off
+rem Get the current directory
+set "CURRENT_DIR=%CD%"
+
+rem Check if the current directory is HOME
+if "%CURRENT_DIR%" NEQ "%HOMEDRIVE%%HOMEPATH%" (
+    echo Current directory is not HOME. Terminating.
+    exit /b
+)
+
+rem Check if PURE_CWD environment variable is set
+if defined PURE_CWD (
+    rem Change to the directory specified in PURE_CWD
+    cd /d "%PURE_CWD%"
+    echo Changed to directory: %PURE_CWD%
+) else (
+    echo PURE_CWD is not set. Staying in the current directory.
+)
